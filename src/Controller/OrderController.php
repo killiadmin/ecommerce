b800e4ae958a778d640eab221d5d1356538b46c1
FILÂTRE Killian <k.filatre@gmail.com>
2025-01-16 12:18:12 +0100

    setup the view list orders and add methode for clean basket

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\OrderDetailsRepository;
 use App\Repository\OrderRepository;
+use App\Service\BasketService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,10 +14,11 @@ class OrderController extends AbstractController
     /**
      * @param OrderRepository $orderRepository
      * @param OrderDetailsRepository $orderDetailsRepository
+     * @param BasketService $basketService
      * @return Response
      */
     #[Route('/order', name: 'app_order')]
-    public function viewLastOrder(OrderRepository $orderRepository, OrderDetailsRepository $orderDetailsRepository): Response
+    public function viewLastOrder(OrderRepository $orderRepository, OrderDetailsRepository $orderDetailsRepository, BasketService $basketService): Response
     {
         $user = $this->getUser();
         if (!$user) {
@@ -27,7 +29,7 @@ class OrderController extends AbstractController
         $lastOrder = $orderRepository->findLastOrderForUser($user);
 
         if (!$lastOrder) {
-            return $this->render('order/order.html.twig', [
+            return $this->render('order/lastOrder.html.twig', [
                 'lastOrder' => null,
                 'products' => [],
             ]);
@@ -36,9 +38,27 @@ class OrderController extends AbstractController
         // Retrieve the products associated with the order
         $products = $orderDetailsRepository->findProductsByOrderId($lastOrder->getId());
 
-        return $this->render('order/order.html.twig', [
+        $basketService->clearBasketForUser($user);
+
+        return $this->render('order/lastOrder.html.twig', [
             'lastOrder' => $lastOrder,
             'products' => $products,
+        ]);
+    }
+
+    #[Route('/list-orders', name: 'app_list-orders')]
+    public function viewListOrders(OrderRepository $orderRepository): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw $this->createAccessDeniedException('Vous devez être connecté pour accéder à cette page.');
+        }
+
+        $orders = $orderRepository->findAllOrdersForUser($user);
+
+        return $this->render('order/listOrders.html.twig', [
+            'orders' => $orders,
         ]);
     }
 }
--- src/Repository/OrderRepository.php
+++ src/Repository/OrderRepository.php
@@ -17,6 +17,10 @@ class OrderRepository extends ServiceEntityRepository
         parent::__construct($registry, Order::class);
     }
 
+    /**
+     * @param User $user
+     * @return Order|null
+     */
     public function findLastOrderForUser(User $user): ?Order
     {
         return $this->createQueryBuilder('o')
@@ -26,5 +30,19 @@ class OrderRepository extends ServiceEntityRepository
             ->setMaxResults(1)
             ->getQuery()
             ->getOneOrNullResult();
+    }
+
+    /**
+     * @param User $user
+     * @return array
+     */
+    public function findAllOrdersForUser(User $user): array
+    {
+        return $this->createQueryBuilder('o')
+            ->andWhere('o.user_order = :user')
+            ->setParameter('user', $user)
+            ->orderBy('o.created_at', 'DESC')
+            ->getQuery()
+            ->getResult();
     }
 }
