0a92598500a9a686ed4bb2817cd13a975c6fb60a
FILÂTRE Killian <k.filatre@gmail.com>
2025-01-17 11:22:42 +0100

    orders details view and others options

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -46,6 +46,10 @@ class OrderController extends AbstractController
         ]);
     }
 
+    /**
+     * @param OrderRepository $orderRepository
+     * @return Response
+     */
     #[Route('/list-orders', name: 'app_list-orders')]
     public function viewListOrders(OrderRepository $orderRepository): Response
     {
@@ -59,6 +63,39 @@ class OrderController extends AbstractController
 
         return $this->render('order/listOrders.html.twig', [
             'orders' => $orders,
+        ]);
+    }
+
+    /**
+     * @param int $id
+     * @param OrderRepository $orderRepository
+     * @param OrderDetailsRepository $orderDetailsRepository
+     * @return Response
+     */
+    #[Route('/order/{id}', name: 'app_order_details')]
+    public function viewOrderDetails(
+        int                    $id,
+        OrderRepository        $orderRepository,
+        OrderDetailsRepository $orderDetailsRepository
+    ): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw $this->createAccessDeniedException('Vous devez être connecté pour accéder à cette page.');
+        }
+
+        $order = $orderRepository->find($id);
+
+        if (!$order || $order->getUserOrder() !== $user) {
+            return $this->redirectToRoute('app_list-orders');
+        }
+
+        $products = $orderDetailsRepository->findProductsByOrderId($id);
+
+        return $this->render('order/detailsOrder.html.twig', [
+            'order' => $order,
+            'products' => $products,
         ]);
     }
 }
